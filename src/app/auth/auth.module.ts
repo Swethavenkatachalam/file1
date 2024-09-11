e2ef273618e9b1596cadb7b0e603fd10390c399a
Abirami1506 <via.abirami15@gmail.com>
2024-09-11 11:55:08 +0530

    fix: changes

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,12 +4,8 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SigupComponent } from './sigup/sigup.component';
-<<<<<<< HEAD
+import { ReactiveFormsModule } from '@angular/forms';
 
-=======
-import { RecipeComponent } from './recipe/recipe.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
->>>>>>> 147e6dc7829035f0a68ffb973a7b95148a4b2564
 
 
 @NgModule({
@@ -20,7 +16,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     AuthRoutingModule,
-    FormsModule,
     ReactiveFormsModule
   ]
 })
